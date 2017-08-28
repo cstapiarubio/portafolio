73c807047468e843b6887cb51168562b2b759209
cstapiarubio <cstapiarubio@gmail.com>
2017-08-28 16:19:01 -0300

    listo portafolio

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 $(window).scroll(function() {
         $('#nav-trans').toggleClass('nav', $(this).scrollTop() > $('#nav-trans').height());
       });
+
+ $(".button-collapse").sideNav({
+  closeOnClick:true,
+  menuWidth:100,
+ });
 });
 
 
